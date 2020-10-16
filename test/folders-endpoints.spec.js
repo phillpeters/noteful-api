735f01e72598c00194de8f5ae32fc6800593928f
Phill Peters <phillpeters1@gmail.com>
2020-10-15 23:35:43 -0600

    prep for heroku deployment

--- test/folders-endpoints.spec.js
+++ test/folders-endpoints.spec.js
@@ -10,7 +10,7 @@ describe('Folders endpoints', () => {
   before(() => {
     db = knex({
       client: 'pg',
-      connection: process.env.TEST_DB_URL
+      connection: process.env.TEST_DATABASE_URL
     });
     app.set('db', db);
   });
